e77fb41b4397b4b4ca8d961c435593a758468485
Marwamu <marwamohamed1293@gmail.com>
2025-06-23 12:27:14 +0300

    create

---
+++ project-2/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navgation from "./Components/Navgation";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navgation />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ project-2/src/Components/Navgation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navgation = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src=".\images\logo.png" alt="" />
+            </div>
+        </nav>
+    );
+}
+
+export default Navgation;
