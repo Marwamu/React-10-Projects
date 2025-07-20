7973324046efbeeec31129a51db4b92e874c0c2c
Marwamu <marwamohamed1293@gmail.com>
2025-07-20 11:35:07 +0300

    update filter style

--- project-4/src/Components/Navagation/Navgation.jsx
+++ project-4/src/Components/Navagation/Navgation.jsx
@@ -6,6 +6,8 @@ const Navgation = ({ setSearchValue, setFilterFoodType   }) => {
     setSearchValue(e.target.value);
   };
   const filterItem = ["All", "Breakfast", "Lunch", "Dinner"];
+  const [activeFilter, setActiveFilter] = useState("All");
+
   return (
     <nav className="navbar">
       <div className="logo-container">
@@ -22,13 +24,16 @@ const Navgation = ({ setSearchValue, setFilterFoodType   }) => {
       <div>
         <ul className="filter-items">
         {filterItem.map((item, index) => (
-          <li
-            key={index}
-            className="filter-item"
-            onClick={() => setFilterFoodType(item)}
-          >
-            {item}
-          </li>
+              <li
+      key={index}
+      className={`filter-item ${activeFilter === item ? "active" : ""}`}
+      onClick={() => {
+        setActiveFilter(item);
+        setFilterFoodType(item); // Call parent handler
+      }}
+    >
+      {item}
+    </li>
         ))}
       </ul>
       </div>
